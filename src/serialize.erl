3c0af32f922266702626c67bd60cbf9a8b9638c8
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-03 22:16:18 +0100

    unit test

--- src/serialize.erl
+++ src/serialize.erl
@@ -10,21 +10,30 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
-
-
 pack(Data) when is_binary(Data) ->
     Size = size(Data),
     {ok, <<Size:32/unsigned-integer, Data/binary>>};
 
 pack(Data) when is_list(Data) ->
-    pack(iolist_to_binary(Data)).
+    pack(iolist_to_binary(Data));
+
+pack(_Data) ->
+    {error, "only list and binary are handled"}.
 
 unpack(Blob) ->
-    <<Size:32/unsigned-integer, Data>> = Blob,
-    io:format("~p ~n ~p", [Size, Data]).
+    <<Size:32/unsigned-integer, Data/binary>> = Blob,
+    case Size == size(Data) of
+        true ->
+            io:format("~p ~n ~p", [Size, Data]),
+            {ok, Data};
+        false ->
+            {error, "wrong size"}
+    end.
 
 -ifdef(EUNIT).
 pack_test() ->
-    Blob = pack("hello"),
-    io:format("~p", [Blob]).
+    {error, _} = pack(42),
+    {ok, Blob} = pack("hello"),
+    {ok, Value} = unpack(Blob),
+    ?assertEqual(<<"hello">>, Value).
 -endif.
